62d5f237e3b8d2151f0630555e0ec92d62736829
Raul Garcia <raul.garcia@ikasle.egibide.org>
2021-02-05 20:02:16 +0100

    Proyecto nuevo

---
+++ Terremotos/Terremotos/TerremotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerremotosApp.swift
+//  Terremotos
+//
+//  Created by  on 05/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TerremotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
